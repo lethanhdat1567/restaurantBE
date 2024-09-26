21bdf145697f3ad6f10bab8522e5ff17dc280417
Le Thanh Dat <lethanhdat1567@gmail.com>
2024-09-26 16:56:55 +0700

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    build: {
+        outDir: "dist", // Đặt thư mục đầu ra là 'dist'
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
