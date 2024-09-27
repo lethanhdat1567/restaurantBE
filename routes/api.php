d2d1ea6d941bbd5a3b0c65c7d7b70336efca3421
Le Thanh Dat <lethanhdat1567@gmail.com>
2024-09-27 22:15:45 +0700

    DOne deploy

--- routes/api.php
+++ routes/api.php
@@ -16,16 +16,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Expr\FuncCall;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 Route::middleware(['auth:sanctum', 'EnsureFrontendRequestsAreStateful'])->group(function() {
     Route::get('logout', [AuthController::class, 'logout']);
     Route::get('user', [AuthController::class, 'getUser']);
